61566b432f148a9e6dfbcbd16da2ed203d59ed4b
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-05-11 09:47:05 +0530

    Update sort012.cpp

--- Linked List/sort012.cpp
+++ Linked List/sort012.cpp
@@ -53,4 +53,66 @@ Node* sortList(Node *head)
 
 // optimal approach
 
+/********************************
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+********************************/
+void insertTail(Node* &tail,Node* curr){
+	tail->next=curr;
+	tail=curr;
+}
+Node* sortList(Node *head)
+{
+        // create three list with dummy nodes
+	Node* zerohead=new Node(-1);
+	Node* zerotail=zerohead;
+	Node* onehead=new Node(-1);
+	Node* onetail=onehead;
+	Node* twohead=new Node(-1);
+	Node* twotail=twohead;
+	
+	Node* curr=head;
+	//insertation in three list
+	while(curr!=NULL){
+		int val=curr->data;
+		if(val==0){
+			insertTail(zerotail,curr);
+		}
+		else if(val==1){
+			insertTail(onetail,curr);
+		}
+		else{
+			insertTail(twotail,curr);
+		}
+		curr=curr->next;
+	}
+	//merge three list
+	if(onehead->next!=NULL){
+		//if onehead list is not empty
+		zerotail->next=onehead->next;
+	}
+	else{
+		//if onehead list is empty
+		zerotail->next=twohead->next;
+	}
+	onetail->next=twohead->next;
+	twotail->next=NULL;
+	// setup head
+	head=zerohead->next;
+	// delete dummy nodes
+	delete zerohead;
+	delete onehead;
+	delete twohead;
+	return head;
+}
 
