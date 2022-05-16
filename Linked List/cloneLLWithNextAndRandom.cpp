9d0afca2020567c344261652c7481482347efecb
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-05-16 15:11:07 +0530

    Update cloneLLWithNextAndRandom.cpp

--- Linked List/cloneLLWithNextAndRandom.cpp
+++ Linked List/cloneLLWithNextAndRandom.cpp
@@ -1,22 +1,5 @@
-// { Driver Code Starts
-#include <bits/stdc++.h>
 
-using namespace std;
-/* Link list Node */
-struct Node {
-    int data;
-    Node *next;
-    Node *arb;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-        arb = NULL;
-    }
-};
-
-
-
+//**time comp O(n) space comp O(n)**
  // } Driver Code Ends
 class Solution
 {   private:
@@ -206,3 +189,65 @@ int main() {
     }
     return 0;
 }  // } Driver Code Ends
+
+
+//**time comp O(n) space comp O(1)**
+class Solution
+{   private:
+    void insertAtTail(Node* &head,Node* &tail,int d){
+        Node* newNode=new Node(d);
+        if(head==NULL){
+            head=newNode;
+            tail=newNode;
+        }
+        else{
+            tail->next=newNode;
+            tail=newNode;
+        }
+    }
+    public:
+    Node *copyList(Node *head)
+    {
+        //Write your code here
+        Node* cloneHead=NULL;
+        Node* cloneTail=NULL;
+        Node* temp=head;
+        while(temp!=NULL){
+            insertAtTail(cloneHead,cloneTail,temp->data);
+            temp=temp->next;
+        }
+        
+        Node* originalNode=head;
+        Node* cloneNode=cloneHead;
+        while(originalNode!=NULL && cloneNode!=NULL){
+            Node* next=originalNode->next;
+            originalNode->next=cloneNode;
+            originalNode=next;
+            
+            next=cloneNode->next;
+            cloneNode->next=originalNode;
+            cloneNode=next;
+        }
+        
+        temp=head;
+        while(temp!=NULL){
+            if(temp->next!=NULL){
+            temp->next->arb=temp->arb ? temp->arb->next:temp->arb;   
+            }
+            temp=temp->next->next;
+        }
+        
+        originalNode=head;
+        cloneNode=cloneHead;
+        while(originalNode!=NULL && cloneNode!=NULL){
+            originalNode->next=cloneNode->next;
+            originalNode=originalNode->next;
+            if(originalNode!=NULL){
+                cloneNode->next=originalNode->next;
+            }
+            cloneNode=cloneNode->next;
+        }
+        return cloneHead;
+    }
+
+};
