e04c84bc0ad0ca9e173ab1379ff922c9f84b1d8a
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-05-17 16:08:47 +0530

    Update temp.cpp

--- Stack/temp.cpp
+++ Stack/temp.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+  stack <int> s;
+  s.push(3);
+  s.push(5);
+  s.push(7);
+  s.pop();
+  s.top();
+  s.empty();
+  s.size();
+}
