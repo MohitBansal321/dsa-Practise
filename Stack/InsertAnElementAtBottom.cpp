bd2a128b310e36b5f8541c676d795472ea3e73fb
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-05-19 16:35:58 +0530

    Create InsertAnElementAtBottom.cpp

---
+++ Stack/InsertAnElementAtBottom.cpp
@@ -0,0 +1,16 @@
+void solve(stack<int>& myStack,int x){
+	if(myStack.empty()){
+		myStack.push(x);
+		return ;
+	}
+	int num=myStack.top();
+	myStack.pop();
+	solve(myStack,x);
+	myStack.push(num);
+}
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    // Write your code here.
+	solve(myStack,x);
+	return myStack;
+}
