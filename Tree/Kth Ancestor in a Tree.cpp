94b17a136021f6688b0939cef05de032b19d5c60
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-06-18 12:59:46 +0530

    Create Kth Ancestor in a Tree.cpp

---
+++ Tree/Kth Ancestor in a Tree.cpp
@@ -0,0 +1,36 @@
+Node* solve(Node *root,int &k,int node){
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data==node){
+        return root;
+    }
+    Node* leftAns=solve(root->left,k,node);
+    Node* rightAns=solve(root->right,k,node);
+    if(leftAns!=NULL && rightAns==NULL){
+        k--;
+        if(k<=0){
+            k=INT_MAX;
+            return root;
+        }
+        return leftAns;
+    }
+    if(leftAns==NULL && rightAns!=NULL){
+        k--;
+        if(k<=0){
+            k=INT_MAX;
+            return root;
+        }
+        return rightAns;
+    }
+    return NULL;
+}
+int kthAncestor(Node *root, int k, int node)
+{
+    Node* ans=solve(root,k,node);
+    if(ans==NULL || ans->data==node){
+        return -1;
+    }
+    else{
+        return ans->data;
+    }
