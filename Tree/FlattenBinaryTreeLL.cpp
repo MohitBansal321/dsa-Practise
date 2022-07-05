bdc09a18254068b0df82329b3782fa689b88e568
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-07-05 19:58:04 +0530

    Create FlattenBinaryTreeLL.cpp

---
+++ Tree/FlattenBinaryTreeLL.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+    void flatten(Node *root)
+    {
+        //code here
+        Node* curr=root;
+        while(curr!=NULL){
+            if(curr->left){
+                Node* pred=curr->left;
+                while(pred->right){
+                    pred=pred->right;
+                }
+                pred->right=curr->right;
+                curr->right=curr->left;
+            }
+            curr=curr->right;
+        }
+        curr=root;
+        while(curr!=NULL){
+            curr->left=NULL;
+            curr=curr->right;
+        }
+    }
+};
