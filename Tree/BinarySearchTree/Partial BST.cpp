5fd980776540dfe993fa94f044150be04d453864
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-07-07 23:56:58 +0530

    Create Partial BST.cpp

---
+++ Tree/BinarySearchTree/Partial BST.cpp
@@ -0,0 +1,17 @@
+bool isBST(BinaryTreeNode<int> *root,int min,int max){
+    if(root==NULL){
+        return true;
+    }
+    if(root->data >=min && root->data<=max){
+        bool left=isBST(root->left,min,root->data);
+        bool right=isBST(root->right,root->data,max);
+        return left && right;
+    }
+    else{
+        return false;
+    }
+}
+bool validateBST(BinaryTreeNode<int> *root) {
+    // Write your code here
+    return isBST(root,INT_MIN,INT_MAX);
+}
