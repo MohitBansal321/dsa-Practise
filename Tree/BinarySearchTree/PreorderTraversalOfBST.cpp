bbbe8e6bad95d5fd5d3c818c4eb7c25c0ac2088f
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-07-12 22:30:51 +0530

    Create PreorderTraversalOfBST.cpp

---
+++ Tree/BinarySearchTree/PreorderTraversalOfBST.cpp
@@ -0,0 +1,19 @@
+BinaryTreeNode<int>* solve(vector<int> &preorder,int mini,int maxi,int &i){
+      if(i>=preorder.size()){
+          return NULL;
+      }   
+     if(preorder[i]<mini || preorder[i]>maxi){
+         return NULL;
+     }
+    BinaryTreeNode<int>* root=new BinaryTreeNode<int>(preorder[i++]);
+    root->left=solve(preorder,mini,root->data,i);
+    root->right=solve(preorder,root->data,maxi,i);
+    return root;
+}
+BinaryTreeNode<int>* preorderToBST(vector<int> &preorder) {
+    // Write your code here.
+    int mini=INT_MIN;
+    int maxi=INT_MAX;
+    int i=0;
+    return solve(preorder,mini,maxi,i);
+}
