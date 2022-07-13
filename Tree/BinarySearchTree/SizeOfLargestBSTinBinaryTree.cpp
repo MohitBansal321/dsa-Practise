2b84a5f354e6a8e92719ef25e301de60ecc62b23
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-07-13 23:24:19 +0530

    Create SizeOfLargestBSTinBinaryTree.cpp

---
+++ Tree/BinarySearchTree/SizeOfLargestBSTinBinaryTree.cpp
@@ -0,0 +1,36 @@
+class info{
+    public:
+    int mini;
+    int maxi;
+    bool isBST;
+    int size;
+};
+info solve(TreeNode<int>* root,int &ans){
+    if(root==NULL){
+        return {INT_MIN,INT_MAX,true,0};
+    }
+    info left=solve(root->left,ans);
+    info right=solve(root->right,ans);
+    info currNode;
+    currNode.size=left.size + right.size + 1;
+    currNode.maxi=max(root->data,right.maxi);
+    currNode.mini=min(root->data,left.mini);
+    if(left.isBST && right.isBST && (root->data>left.maxi && root->data<right.mini)){
+        currNode.isBST=true;
+    }
+    else{
+        currNode.isBST=false;
+    }
+    if(currNode.isBST){
+        ans=max(ans,currNode.size);
+        cout<<ans<<"ans is";
+    }
+    return currNode;
+}
+int largestBST(TreeNode<int>* root) 
+{
+    // Write your code here.
+    int maxSize=0;
+    info temp=solve(root,maxSize);
+    return maxSize;
+}
