67e503cf832944c3d2865b2f796f2c4b28c716cb
codermohit302 <78220157+codermohit302@users.noreply.github.com>
2022-07-24 12:25:55 +0530

    Update Kth Largest Sum Subarray.cpp

--- Heap/Kth Largest Sum Subarray.cpp
+++ Heap/Kth Largest Sum Subarray.cpp
@@ -1,3 +1,4 @@
+// apporach one tc 0(n^2(longn)) Sc 0(n^2)
 #include<algorithm>
 int getKthLargest(vector<int> &arr, int k)
 { 
@@ -16,4 +17,26 @@ int getKthLargest(vector<int> &arr, int k)
     sort(sumStore.begin(),sumStore.end() );
   // return that kth value
     return sumStore[sumStore.size()-k];
+}
+
+// approach second tc 0( n^2 (longn) ) Sc 0(k)
+#include<bits/stdc++.h>
+int getKthLargest(vector<int> &arr, int k)
+{    priority_queue<int,vector<int>,greater<int>> pq;
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+            sum+=arr[j];
+            if(pq.size()<k){
+                pq.push(sum);
+                
+            }
+            else{  
+            pq.push(sum);
+            pq.pop();
+            }
+        }
+    }
+     return pq.top();
 }
