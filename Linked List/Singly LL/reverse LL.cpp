8ce6deaca49cbc9f1563d0015f84405557326fe1
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-09-19 20:27:33 +0530

    Create reverse LL.cpp

---
+++ Linked List/Singly LL/reverse LL.cpp
@@ -0,0 +1,12 @@
+**Interative apporach**
+ListNode* reverseList(ListNode* head) {
+        ListNode* nextNode;
+        ListNode* prevNode=NULL;
+        while(head){
+            nextNode=head->next;
+            head->next=prevNode;
+            prevNode=head;
+            head=nextNode;
+        }
+        return prevNode;
+    }
