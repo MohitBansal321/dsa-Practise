e41f5a12aafffcb90893e5bc66e29e93071572aa
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-10-10 01:03:07 +0530

    Update mergeTwoSortedLL.cpp

--- Linked List/mergeTwoSortedLL.cpp
+++ Linked List/mergeTwoSortedLL.cpp
@@ -1,4 +1,4 @@
-approach 1
+// approach 1
 if(head1==NULL) return head2;
     if(head2==NULL) return head1;
     Node* new_list=NULL;
@@ -42,31 +42,7 @@ if(head1==NULL) return head2;
     }
     return head_ref;
 
-apraoch 2
-#include <bits/stdc++.h> 
-/************************************************************
-
-    Following is the linked list node structure.
-    
-    template <typename T>
-    class Node {
-        public:
-        T data;
-        Node* next;
-
-        Node(T data) {
-            next = NULL;
-            this->data = data;
-        }
-
-        ~Node() {
-            if (next != NULL) {
-                delete next;
-            }
-        }
-    };
-
-************************************************************/
+//  Approach 2
 Node<int>* solve(Node<int>*first,Node<int>* second){
 	if(first->next==NULL){
 		first->next=second;
