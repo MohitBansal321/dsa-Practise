97f9c78b3d6f52486efb7574d7412d5746e9638f
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-10-15 13:39:21 +0530

    New easy to understand code

--- Linked List/cloneLLWithNextAndRandom.cpp
+++ Linked List/cloneLLWithNextAndRandom.cpp
@@ -1,253 +1,32 @@
-
-//**time comp O(n) space comp O(n)**
- // } Driver Code Ends
-class Solution
-{   private:
-    void insertAtTail(Node* &head,Node* &tail,int d){
-        Node* newNode=new Node(d);
-        if(head==NULL){
-            head=newNode;
-            tail=newNode;
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* curr=head;
+        while(curr){
+            Node* temp=curr->next;
+            curr->next=new Node(curr->val);
+            curr->next->next=temp;
+            curr=temp; 
         }
-        else{
-            tail->next=newNode;
-            tail=newNode;
+        
+        curr=head;
+        while(curr){
+            if(curr->random)
+                curr->next->random=curr->random->next;
+            curr=curr->next->next;  // go to the next orignal node
         }
-    }
-    public:
-    Node *copyList(Node *head)
-    {
-        //Write your code here
-        Node* cloneHead=NULL;
-        Node* cloneTail=NULL;
-        Node* temp=head;
-        while(temp!=NULL){
-            insertAtTail(cloneHead,cloneTail,temp->data);
+        Node* ans=new Node(0);   // dummy node for ans
+        Node* temp=ans;         // temp node for reference pointer
+        while(head){
+            // copy of alernative added node from orignal list
+            temp->next=head->next;
             temp=temp->next;
-        }
-        
-        unordered_map<Node*,Node*> addRandom;
-        Node* orgNode=head;
-        Node* cloneNode=cloneHead;
-        while(orgNode!=NULL){
-            addRandom[orgNode]=cloneNode;
-            orgNode=orgNode->next;
-            cloneNode=cloneNode->next;
-        }
-        orgNode=head;
-        cloneNode=cloneHead;
-        while(orgNode!=NULL){
-            cloneNode->arb=addRandom[orgNode->arb];
-            orgNode=orgNode->next;
-            cloneNode=cloneNode->next;
-        }
-        return cloneHead;
-    }
-
-};
-
-// { Driver Code Starts.
-
-
-void print(Node *root) {
-    Node *temp = root;
-    while (temp != NULL) {
-        int k;
-        if (temp->arb == NULL)
-            k = -1;
-        else
-            k = temp->arb->data;
-        cout << temp->data << " " << k << " ";
-        temp = temp->next;
-    }
-}
-
-
-void append(Node **head_ref, Node **tail_ref, int new_data) {
-
-    Node *new_node = new Node(new_data);
-    if (*head_ref == NULL) {
-        *head_ref = new_node;
-    } else
-        (*tail_ref)->next = new_node;
-    *tail_ref = new_node;
-}
-
-bool validation(Node *head, Node *res) {
-
-
-    Node *temp1 = head;
-    Node *temp2 = res;
-
-    int len1 = 0, len2 = 0;
-    while (temp1 != NULL) {
-        len1++;
-        temp1 = temp1->next;
-    }
-    while (temp2 != NULL) {
-        len2++;
-        temp2 = temp2->next;
-    }
-
-    /*if lengths not equal */
-
-    if (len1 != len2) return false;
-
-    temp1 = head;
-    temp2 = res;
-    map<Node*,Node*> a;
-    while (temp1 != NULL) {
-        
-        if(temp1==temp2)
-            return false;
-        
-        if (temp1->data != temp2->data) return false;
-        if (temp1->arb != NULL and temp2->arb != NULL) {
-            if (temp1->arb->data != temp2->arb->data)
-                return false;
-        } else if (temp1->arb != NULL and temp2->arb == NULL)
-            return false;
-          else if (temp1->arb == NULL and temp2->arb != NULL)
-            return false;
-        a[temp1]=temp2;
-        temp1 = temp1->next;
-        temp2 = temp2->next;
-    }
-    
-
-    temp1 = head;
-    temp2 = res;
-    while (temp1 != NULL) {
-        
-        if (temp1->arb != NULL and temp2->arb != NULL) {
-            if (a[temp1->arb] != temp2->arb) return false;
-        }
-        temp1 = temp1->next;
-        temp2 = temp2->next;
-    }
-    return true;
-}
-
-
-
-int main() {
-
-    int T, i, n, l, k;
-    Node *generated_addr = NULL;
-     /*reading input stuff*/
-    cin >> T;
-    while (T--) {
-        generated_addr = NULL;
-        struct Node *head = NULL, *tail = NULL;
-        struct Node *head2 = NULL, *tail2 = NULL;
-        cin >> n >> k;
-        for (i = 1; i <= n; i++) {
-            cin >> l;
-            append(&head, &tail, l);
-            append(&head2, &tail2, l);
-        }
-         for (int i = 0; i < k; i++) {
-            int a, b;
-            cin >> a >> b;
-
-            Node *tempA = head;
-            Node *temp2A = head2;
-            int count = -1;
-
-            while (tempA != NULL) {
-                count++;
-                if (count == a - 1) break;
-                tempA = tempA->next;
-                temp2A = temp2A->next;
-            }
-            Node *tempB = head;
-            Node *temp2B = head2;
-            count = -1;
-
-            while (tempB != NULL) {
-                count++;
-                if (count == b - 1) break;
-                tempB = tempB->next;
-                temp2B = temp2B->next;
-            }
-
-            // when both a is greater than N
-            if (a <= n){
-                tempA->arb = tempB;
-                temp2A->arb = temp2B;
-            }
-        }
-        /*read finished*/
-
-        generated_addr = head;
-        Solution ob;
-        struct Node *res = ob.copyList(head);
-        if(validation(head2,res)&&validation(head,res))
-            cout << validation(head2, res) << endl;
-        else
-            cout << 0 << endl;
-    }
-    return 0;
-}  // } Driver Code Ends
-
-
-//**time comp O(n) space comp O(1)**
-class Solution
-{   private:
-    void insertAtTail(Node* &head,Node* &tail,int d){
-        Node* newNode=new Node(d);
-        if(head==NULL){
-            head=newNode;
-            tail=newNode;
-        }
-        else{
-            tail->next=newNode;
-            tail=newNode;
-        }
-    }
-    public:
-    Node *copyList(Node *head)
-    {
-        //Write your code here
-        Node* cloneHead=NULL;
-        Node* cloneTail=NULL;
-        Node* temp=head;
-        while(temp!=NULL){
-            insertAtTail(cloneHead,cloneTail,temp->data);
-            temp=temp->next;
-        }
-        
-        Node* originalNode=head;
-        Node* cloneNode=cloneHead;
-        while(originalNode!=NULL && cloneNode!=NULL){
-            Node* next=originalNode->next;
-            originalNode->next=cloneNode;
-            originalNode=next;
             
-            next=cloneNode->next;
-            cloneNode->next=originalNode;
-            cloneNode=next;
-        }
-        
-        temp=head;
-        while(temp!=NULL){
-            if(temp->next!=NULL){
-            temp->next->arb=temp->arb ? temp->arb->next:temp->arb;   
-            }
-            temp=temp->next->next;
-        }
-        
-        originalNode=head;
-        cloneNode=cloneHead;
-        while(originalNode!=NULL && cloneNode!=NULL){
-            originalNode->next=cloneNode->next;
-            originalNode=originalNode->next;
-            if(originalNode!=NULL){
-                cloneNode->next=originalNode->next;
-            }
-            cloneNode=cloneNode->next;
-        }
-        return cloneHead;
-    }
+            // restoring orignal list
+            head->next=head->next->next;
+            head=head->next;
 
+        }
+        return ans->next;
+    }
 };
