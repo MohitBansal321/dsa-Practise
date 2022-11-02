50a8f0098252b11b80e8f4b3aec907485c819ed6
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-11-02 19:24:51 +0530

    Create AdjacencyMatrix.cpp

---
+++ Graph/Graph Striver/AdjacencyMatrix.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int adj[n+1][m+1]={0};
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+        
+    }
+    return 0;
+}
