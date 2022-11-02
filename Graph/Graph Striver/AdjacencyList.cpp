94394ec1fe04811a84dafe10892cc1e69fd42a35
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-11-02 19:35:16 +0530

    Create AdjacencyList.cpp

---
+++ Graph/Graph Striver/AdjacencyList.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[n+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);    /// when graph is directed then this line will commented
+    }
+    return 0;
+}
