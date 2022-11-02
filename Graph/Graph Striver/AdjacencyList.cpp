1f7a62fee581f85952d03dd093d3e7b401f936fe
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-11-02 20:00:27 +0530

    adding weighted graph code

--- Graph/Graph Striver/AdjacencyList.cpp
+++ Graph/Graph Striver/AdjacencyList.cpp
@@ -11,5 +11,16 @@ int main()
         adj[u].push_back(v);
         adj[v].push_back(u);    /// when graph is directed then this line will commented
     }
+
+    // when graph is weighted
+    int n,m;
+    cin>>n>>m;
+    vector<pair<int,int>> adj[n+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);    /// when graph is directed then this line will commented
+    }
     return 0;
 }
