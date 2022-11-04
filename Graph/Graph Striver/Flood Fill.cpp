0817e1e792095385091279e2ade9298bea814cc0
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-11-05 00:07:42 +0530

    Create Flood Fill.cpp

---
+++ Graph/Graph Striver/Flood Fill.cpp
@@ -0,0 +1,25 @@
+class Solution {
+ private:
+    void dfs(int row,int col,vector<vector<int>>& ans,vector<vector<int>>& image,int newColor, int delRow[],int delCol[],int initalColor){
+        ans[row][col]=newColor;
+        int n=image.size();
+        int m=image[0].size();
+        for(int i=0;i<4;i++){
+            int nrow=row+delRow[i];
+            int ncol=col+delCol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==initalColor && ans[nrow][ncol]!=newColor){
+                dfs(nrow,ncol,ans,image,newColor,delRow,delCol,initalColor);               
+            }
+        }
+        
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int initalColor=image[sr][sc];
+        vector<vector<int>>ans=image;
+        int delRow[]={-1,0,+1,0};
+        int delCol[]={0,+1,0,-1};
+        dfs(sr,sc,ans,image,newColor,delRow,delCol,initalColor);
+        return ans;
+    }
+};
