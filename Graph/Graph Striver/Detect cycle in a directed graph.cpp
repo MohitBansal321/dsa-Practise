86d536809cc15fce14591cd6eb455c03b7ae9f29
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-11-10 23:18:53 +0530

    adding one more approach

--- Graph/Graph Striver/Detect cycle in a directed graph.cpp
+++ Graph/Graph Striver/Detect cycle in a directed graph.cpp
@@ -29,3 +29,35 @@ class Solution {
        return false;
     }
 };
+
+// -----------------------------------------------------ONLY VISTED ARRAY--------------------------------------------------------------
+
+class Solution {
+  private:
+    bool dfs(int node,vector<int> adj[],int vis[]){
+        vis[node]=2;
+        for(auto it:adj[node]){
+            //when adjcent  node is not visited
+            if(!vis[it]){
+                if(dfs(it,adj,vis)) return true;
+            }
+            // when visited adjcent node
+            else if(vis[it]==2) return true;
+        }
+        vis[node]=1;
+        return false;
+    }
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+       int visited[V]={0};
+       for(int i=0;i<V;i++){
+           if(!visited[i]){
+               if(dfs(i,adj,visited)){
+                   return true;
+               }
+           }
+       }
+       return false;
+    }
+};
