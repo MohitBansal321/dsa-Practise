5d3c98db2152861b9d49a028d5044a42b81836c0
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2022-11-11 20:26:35 +0530

    Update Detect cycle in a directed graph.cpp

--- Graph/Graph Striver/Detect cycle in a directed graph.cpp
+++ Graph/Graph Striver/Detect cycle in a directed graph.cpp
@@ -61,3 +61,42 @@ class Solution {
        return false;
     }
 };
+
+
+// -------------------------------------------------------------- TopoSort kahn's algo using BFS --------------------------------------
+
+
+
+
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        int indeg[V]={0};
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i]){
+	            indeg[it]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i=0;i<V;i++){
+	        if(indeg[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    vector<int> topo;
+	    while(!q.empty()){
+	        int front=q.front();
+	        q.pop();
+	        topo.push_back(front);
+	        for(auto i:adj[front]){
+	            indeg[i]--;
+	            if(indeg[i]==0) q.push(i);
+	        }
+	    }
+	    if(topo.size()!=V){
+	        return true;
+	    }
+	    return false;
+    }
+};
