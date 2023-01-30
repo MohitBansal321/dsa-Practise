6b8a20fa13709e792f17359d392af004da48d293
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-01-30 19:37:37 +0530

    LeetCode 102

---
+++ Tree/Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+//         IF root is Null in starting
+        if(root==NULL){
+            return ans;
+        }
+        
+//         Used for storing left and right nodes
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int count=q.size();
+            vector<int> temp;
+            for(int i=0;i<count;i++)
+            {
+                TreeNode* front=q.front();
+                q.pop();
+                temp.push_back(front->val);
+                if(front->left!=NULL)
+                    q.push(front->left);
+                if(front->right!=NULL)
+                    q.push(front->right);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
