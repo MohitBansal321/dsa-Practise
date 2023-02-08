2698c66892050957ceaaed351094627bab2dd99d
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-02-09 01:46:20 +0530

    Add efficient approach

--- Tree/CheckForBalancedTree.cpp
+++ Tree/CheckForBalancedTree.cpp
@@ -1,4 +1,4 @@
-// time complx O(n^2) due to height function call
+// time complex O(n^2) due to height function call
 class Solution{
     public:
     //Function to check whether a binary tree is balanced or not.
@@ -29,35 +29,19 @@ class Solution{
     }
 };
 
-// time complex
-class Solution{
-    private:
-    pair<bool,int> isBalancedfast(Node* root){
-        if(root==NULL){
-            pair<bool,int> p=make_pair(true,0);
-            return p;
-        }
-        pair<bool,int> left=isBalancedfast(root->left);
-        pair<bool,int> right=isBalancedfast(root->right);
-        bool leftans=left.first;
-        bool rightans=right.first;
-        bool diff=abs(left.second-right.second)<=1;
-        pair<bool,int> ans;
-        ans.second=max(left.second,right.second) + 1;
-        if(leftans && rightans && diff){
-            ans.first=true;
-        }
-        else{
-            ans.first=false;
-        }
-        return ans;
+// time complex O(n)
+class Solution {
+public:
+    int isHeight(TreeNode* root){
+        if(root==NULL) return 0;
+        int left=isHeight(root->left);
+        if(left==-1) return -1;
+        int right=isHeight(root->right);
+        if(right==-1) return -1;
+        if(abs(left-right)>1) return -1;
+        return max(left,right)+1;
     }
-    public:
-    //Function to check whether a binary tree is balanced or not.
-    bool isBalanced(Node *root)
-    {
-        //  Your Code here
-        return isBalancedfast(root).first;
-        
+    bool isBalanced(TreeNode* root) {
+        return isHeight(root)!=-1;
     }
 };
