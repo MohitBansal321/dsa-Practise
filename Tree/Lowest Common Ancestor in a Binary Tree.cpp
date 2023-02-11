22f828691162535c7397cdd079cf3707ec8c338c
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-02-12 02:00:27 +0530

    Leetcode

--- Tree/Lowest Common Ancestor in a Binary Tree.cpp
+++ Tree/Lowest Common Ancestor in a Binary Tree.cpp
@@ -1,29 +1,12 @@
-class Solution
-{
-    public:
-    //Function to return the lowest common ancestor in a Binary Tree.
-    Node* lca(Node* root ,int n1 ,int n2 )
-    {
-       //Your code here 
-       if(root==NULL){
-           return NULL;
-       }
-       if(root->data == n1 || root->data == n2){
-           return root;
-       }
-       Node* leftAns=lca(root->left,n1,n2);
-       Node* rightAns=lca(root->right,n1,n2);
-       if(leftAns!=NULL && rightAns!=NULL){
-           return root;
-       }
-       else if(leftAns!=NULL && rightAns==NULL){
-           return leftAns;
-       }
-       else if(leftAns==NULL && rightAns!=NULL){
-           return rightAns;
-       }
-       else{
-           return NULL;
-       }
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL || root==p || root==q) return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if(left==NULL) return right;
+        else if(right==NULL) return left;
+//         When the left and right not null we found our result
+        else return root;
     }
 };
