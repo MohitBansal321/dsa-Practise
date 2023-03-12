a862a4f1072de2ff28236e6f06f2c8b5b9ea81ac
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-03-12 23:59:40 +0530

    GFG

---
+++ Dp/Subset Sum Problem (knapsack variation).cpp
@@ -0,0 +1,25 @@
+// -------------------------------Top-Down Approach------------------------------
+class Solution{   
+public:
+    bool isSubsetSum(vector<int>arr, int sum){
+        int n=arr.size();
+        bool t[n+1][sum+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0) t[i][j]=false;
+                if(j==0) t[i][j]=true;
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(arr[i-1]<=j){
+                    t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];    // 1 || 0 = 1    0 || 0 = 0
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
+    }
+};
