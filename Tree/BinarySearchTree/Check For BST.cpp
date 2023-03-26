de784c11ff5e5b9d1fe01169e1d2b05f30f7ceae
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-03-26 22:45:45 +0530

    GFG

---
+++ Tree/BinarySearchTree/Check For BST.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to check whether a Binary Tree is BST or not.
+    bool solve(Node* root,int min,int max){
+        if(root==NULL) return true;
+        if(root->data>min && root->data<max){
+            bool left=solve(root->left,min,root->data);
+            bool right=solve(root->right,root->data,max);
+            return (left && right);
+        }
+        else{
+            return false;
+        }
+    }
+    bool isBST(Node* root) 
+    {
+        return solve(root,INT_MIN,INT_MAX);
+    }
+};
