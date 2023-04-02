266ba5cd1e29532331f370b5892051fb6f50b3fd
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-04-02 18:28:07 +0530

    LEETCODE

--- String/Find the Index of the First Occurrence in a String.cpp
+++ String/Find the Index of the First Occurrence in a String.cpp
@@ -71,3 +71,27 @@ public:
         return -1;
     }
 };
+
+// Sliding Window 
+
+int strStr(string haystack, string needle) {
+        string s = "";
+        int i = 0;
+        int j =0;
+        int k = needle.size();
+        while(j<haystack.size()){
+            s +=haystack[j];
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
+                if(s==needle){
+                    return i;
+                }
+                s.erase(0, 1);
+                i++;
+                j++;
+            }
+        }
+        return -1;
+    }
