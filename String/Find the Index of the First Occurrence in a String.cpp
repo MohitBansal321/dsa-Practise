05d7d63da200cb94f57157adc78bc07e9f0c0bcc
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-04-02 16:34:41 +0530

    LEETCODE

--- String/Find the Index of the First Occurrence in a String.cpp
+++ String/Find the Index of the First Occurrence in a String.cpp
@@ -1,3 +1,4 @@
+// Brute Force
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -12,6 +13,59 @@ public:
             }
             if(j==m){
                 return i;
+            }
+        }
+        return -1;
+    }
+};
+
+// KMP ALGO
+
+class Solution {
+public:
+    void computeLPS(string pattern,int n,vector<int>&lps){
+        int i=1;
+        int len=0;
+        lps[0]=0;
+        while(i<n){
+            if(pattern[i]==pattern[len]){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+    }
+    int strStr(string haystack, string needle) {
+        int m=haystack.length();
+        int n=needle.length();
+        if(n==0) return -1;
+        if(n==0 && m==0) return -1;
+        if(n>m) return -1;
+        vector<int>lps(n);
+        computeLPS(needle,n,lps);   // calculate pattern repeating element in needle
+        int i=0,j=0;
+        while(i<m){
+            if(needle[j]==haystack[i]){
+                i++;
+                j++;
+            }
+            if(j==n) return i-j;
+            if(i<m && needle[j]!=haystack[i]){
+                if(j!=0){
+                    j=lps[j-1];
+                }
+                else{
+                    i++;
+                }
             }
         }
         return -1;
