aef704cc32bc1947df08cd1fd5e8f420a7e756c3
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-04-10 11:12:20 +0530

    LEETCODE

---
+++ Array/Jump Game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size()==1) return true;
+        int reach=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>reach) return false;
+            reach=max(reach,i+nums[i]);
+        }
+        return true;
+    }
+};
