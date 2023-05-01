17fcd4291dee9288b34bc5089ff5b1e9d32e09d3
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-05-02 02:18:44 +0530

    GFG

--- Tree/Binary Tree/SumTree.cpp
+++ Tree/Binary Tree/SumTree.cpp
@@ -1,34 +1,21 @@
- class Solution
-{   private:
-    pair<bool,int> isSumTreee(Node* root){
-        if(root==NULL){
-            pair<bool,int> p=make_pair(true,0);
-            return p;
-        }
-        if(root->left==NULL && root->right==NULL){
-            pair<bool,int> p=make_pair(true,root->data);
-            return p;
-        }
-        pair<bool,int> leftAns=isSumTreee(root->left);
-        pair<bool,int> rightAns=isSumTreee(root->right);
-        
-        bool isleftsumtree=leftAns.first;
-        bool isrightsumtree=rightAns.first;
-        bool condn=root->data==leftAns.second+rightAns.second;
-        pair<bool,int> ans;
-        if(isleftsumtree && isrightsumtree && condn){
-            ans.first=true;
-            ans.second=root->data+leftAns.second+rightAns.second;
-        }
-        else{
-            ans.first=false;
-        }
-        return ans;
-    }
+class Solution
+{
     public:
+    bool res=true;
+    int sum(Node* root){
+        if(root==NULL) return 0;
+        int left=sum(root->left);
+        int right=sum(root->right);
+        if(root->left==NULL && root->right==NULL) return root->data;
+        if(root->data!=left+right){
+            res=false;
+        }
+        return left+right+root->data;
+    }
     bool isSumTree(Node* root)
     {
-         // Your code here
-         return isSumTreee(root).first;
+         if(root==NULL) return true;
+         sum(root);
+         return res;
     }
 };
